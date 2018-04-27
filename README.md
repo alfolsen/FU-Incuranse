b6caf5752977aceef638d47a4e81c0c43b30cb01
bjornum <37101863+bjornum@users.noreply.github.com>
2018-04-27 11:22:40 +0200

    .

--- README.md
+++ README.md
@@ -15,3 +15,14 @@ Admin del-
 Dashboard - oversikt over salgs data
 Produkt liste - legge inn å slette produkt
 Salgs side - oversikt over kunder og salg
+
+
+--------------------------------
+Get Icons, by simply copying the "code" into text.
+
+for example:  type &spades; as text, then it will become the spade icon.
+
+<p> &spades; </p>
+
+https://dev.w3.org/html5/html-author/charref
+
--- js/bjorn.js
+++ js/bjorn.js
@@ -1,5 +1,5 @@
 //id for modal trykk
-var lifePush = document.getElementById('lifeLine');
+var lifePush = document.querySelectorAll('.infoBox');//getElementById('lifeLine');
 var torPush = document.getElementById('torpedoGoAway');
 
 //to get modal
@@ -7,11 +7,19 @@ var modal = document.getElementById('myModal');
 var modal2 = document.getElementById('myModal2');
 
 
-
-
-lifePush.onclick = function() {
-    modal.style.display = "block";
+for (let index = 0; index < lifePush.length; index++) {
+    const element = lifePush[index];
+    
+    element.onclick = function() {
+        /* Switch: Targets element.id, toggles .modal-element.id (.hide|.show) classes  */ 
+        
+        //modal.style.display = "block";
+    }
 }
+
+/*
+
+
 
 
 window.onclick = function(event) {
@@ -19,25 +27,23 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+*/
 
-torPush.onclick = function() {
-    modal2.style.display = "block";
-}
 
 
 //collapsible
 
-var collaps = document.getElementsByClassName("dropmenu1");
+var collaps = document.getElementsByClassName("dropmenu");
 var i;
 
 for(i = 0; i < collaps.lenght; i++) {
     collaps[i].addEventListener("click", function() {
         this.classList.toggle("active");
-        var dropcontent1 = this.nextElementSibling;
-        if(dropcontent1.style.display === "block") {
-            dropcontent1.style.display="none";
+        var dropcontent = this.nextElementSibling;
+        if(dropcontent.style.display === "block") {
+            dropcontent.style.display="none";
         } else {
-            dropcontent1.style.display="block";
+            dropcontent.style.display="block";
         }
     });
 }
